e2d2a3cd7cf9675b13715ab0469a00125f96464e
Jeff Innes <jeffdango@gmail.com>
2018-11-15 15:23:03 -0700

    Added remaining (currently planned) selectors

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const millsecInDay = (24 * 60 * 60 * 1000);
 const startDate = document.querySelector('#start-date');
 const endDate = document.querySelector('#renewal-date');
 const daysLeft = document.querySelector('#days-left');
+const perYearCost = document.querySelector("#single-cost-per-year");
+const perDayCost = document.querySelector('#single-cost-per-day');
+const proratedSingle = document.querySelector('#prorated-single');
+const qty = document.querySelector('#qty');
+const subtotal = document.querySelector('#subtotal');
+const tax = document.querySelector('#tax');
+const total = document.querySelector('#total');
 
 // Define current date
 let currentDate = new Date();
@@ -14,7 +21,7 @@ let curDay = currentDate.getDate();
 // Set Start date to current date
 startDate.setAttribute('value', `${curYear}-${curMonth}-${curDay}`);
 
-// Define event listeners
+// Misc. Function Declarations
 function calcDate (start, end) {
   const sDate = new Date(start);
   const eDate = new Date(end);
@@ -22,6 +29,7 @@ function calcDate (start, end) {
   return dateDiff;
 };
 
+// Define event listeners
 startDate.addEventListener('input', () => {
    let days = calcDate(document.querySelector('#start-date').value, document.querySelector('#renewal-date').value);
 
@@ -33,3 +41,4 @@ endDate.addEventListener('input', () => {
 
   daysLeft.innerHTML = days;
 });
+
